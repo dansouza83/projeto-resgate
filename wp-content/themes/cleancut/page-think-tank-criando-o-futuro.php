7bfa9022538bb46cfd10d810986b5ecc5896c047
Dan <souzadanbr@gmail.com>
2021-02-11 21:47:26 -0800

    Dynamic videos custom  post type with some css by Daniel Souza

--- wp-content/themes/cleancut/page-think-tank-criando-o-futuro.php
+++ wp-content/themes/cleancut/page-think-tank-criando-o-futuro.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="showcase-think-tank-criando-o-futuro">
+<div class="showcase-think-tank-criando-o-futuro">
  <div class="showcase-criando-o-futuro-content">
   <div class="showcase-criando-o-futuro-buttons">
    <?php if (get_theme_mod('queroestudar_url', 'https://projetoresgate.org.br/quero-estudar-projeto-resgate/') != '') : ?>
@@ -12,24 +12,25 @@
    <?php endif; ?>
   </div>
  </div>
-</section>
+</div>
 
-<section>
+<div>
  <img class="img-responsive" src="<?php echo get_theme_file_uri('/img/ThinkTank-Estudantes3-1.jpg') ?>" alt="">
-</section>
-
-<section>
  <div class="col-md-6">
   <img src="<?php echo get_theme_file_uri('/img/depoimento.jpg') ?>" alt="">
  </div>
  <div class="col-md-6">
   <img src="<?php echo get_theme_file_uri('/img/depoimento2.jpg') ?>" alt="">
  </div>
-</section>
-
-<section>
+</div>
 
 
+
+<div><h1 class="text-center">VÍDEOS</h1></div>
+
+
+
+ <?php $i = 0; ?>
 
  <?php if (have_posts()) :  ?>
   <?php while (have_posts()) : the_post(); ?>
@@ -46,20 +47,32 @@
    $loop = new WP_Query($args);
 
    while ($loop->have_posts()) : $loop->the_post(); ?>
-    <article class="post">
+    <?php
+    $i++;
+    if ($i % 2 != 0) {
+     $float = 'no-float';
+     $bgcolor = 'bg-video-gr';
+    } else {
+     $float = 'pull-right';
+     $bgcolor = 'bg-color-org';
+    }
+    ?>
+    <div class="container-flex">
+     <article class="post <?php echo $bgcolor; ?>">
+      <div class="col-md-6 no-padding <?php echo $float ?>">
+       <div class="embed-container">
+        <?php the_field('think_tank_video') ?>
+       </div>
+      </div>
+      <div class="col-md-6">
+       <h3><?php the_title(); ?></h3>
+       <?php the_content(); ?>
+      </div>
+
+     </article>
+    </div>
     <hr>
 
-     <div class="col-md-5">
-      <div class="embed-container">
-       <?php the_field('think_tank_video') ?>
-      </div>
-     </div>
-     <div class="col-md-7">
-      <h3><?php the_title(); ?></h3>
-      <?php the_content(); ?>
-     </div>
-
-    </article>
    <?php endwhile;
    wp_reset_postdata(); ?>
 
@@ -68,7 +81,7 @@
 
  <?php endif; ?>
 
-</section>
+</div>
 
 
 
